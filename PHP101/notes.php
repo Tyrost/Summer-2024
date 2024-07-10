42056b77b6310a370570fe95871654510c19c111
Tyrost <ccorzo306@gmail.com>
2024-07-10 00:25:52 -0700

    Applied PHP with SQL database for simple data posts.

---
+++ PHP101/notes.php
@@ -0,0 +1,98 @@
+<?php
+
+// ------------------------------------------------------------------------------->
+
+
+    // $capitals = array ("USA" => "Washington D.C.",
+    //                    "Japan" => "Tokyo",
+    //                    "South Korea" => "Seoul",
+    //                    "India" => "New Delhi");
+
+    // foreach($capitals as $i => $value) {
+    //     echo"$i = $value <br>";
+    // }
+
+    // if (isset($_POST["username"])) {
+
+    //     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    // }
+
+    // setcookie("fav_food", "coffee", time() + 86400 * 2, "/");
+    // setcookie("fav_food", "pizza", time() + 86400 * 5, "/");
+    // setcookie("fav_food", "hamburger", time() + 86400 * 9, "/");
+  
+    // foreach($_COOKIE as $key => $value) {
+    //     echo"$key = $value <br>"; 
+    // }
+
+    // $_SESSION["title"] = "ALPHA DESIGNS";
+    // if (!empty($_SESSION['username'])) {
+    //     echo$_SESSION['username'] . "<br>";
+    // }  else {
+    //     echo"null <br>";
+    // }
+    
+
+    // if (isset($_POST['login'])) {
+    //     if (isset($_POST["username"])) {
+    //         $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    //         $_SESSION['username'] = $_POST['username'];
+    //         header('Location: home.php');
+
+    //     } else {
+    //         header('Location: index.php');
+    //     }
+    // } else {
+    //     echo "Not yet implemented";
+    // }
+    
+    // $_SESSION['username'] = "DanielCorzo";
+
+    // foreach($_SERVER as $key => $value) {
+    //     echo "{$key} = {$value} <br>";
+    // }
+
+    // $password = "pizza123";
+
+    // $hash = password_hash($password, PASSWORD_ARGON2ID);
+
+    // $attempt = 'pizza123';
+
+    // function verify($attempt, $hash) {
+    //     return (password_verify($attempt, $hash));
+
+    // }
+
+    // echo verify($attempt, $hash);
+
+    // $sql = "INSERT INTO users (username, password) #INSERT INTO {database name} (fields to insert; name, password, email, etc...) VALUES (values for each named field)
+    //         VALUES ('Sandra', 'yellow123')";
+    
+    // try {
+    //     mysqli_query($conn, $sql);
+    //     echo 'User is now registered';
+    // }
+
+    // catch(mysqli_sql_exception) {
+    //     echo'';
+    // }
+
+    // $sql = "SELECT * FROM users";//WHERE user_id = 2";
+    // $result = mysqli_query($conn, $sql);
+    
+    // if (mysqli_num_rows($result) > 0) {
+    //     while($row = mysqli_fetch_assoc($result)) {
+    //         print_r($row);
+    //     }
+
+    // } else {
+    //     echo"No data found with described properties";
+    // }
+
+    // mysqli_close($conn);
+
+// ------------------------------------------------------------------------------->
+
+?>

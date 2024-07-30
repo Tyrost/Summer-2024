b20742f31c48dc53f7b9214814faa91ce100a3e2
Tyrost <ccorzo306@gmail.com>
2024-07-30 02:14:53 -0700

    Uodates and refactoring. `del_asset` function in progress (non-functional). master asset tracker system not working as a whole.

--- API101/Open Trivia API/assets/components.py
+++ API101/Open Trivia API/assets/components.py
@@ -2,18 +2,10 @@ import pygame
 import logging as log
 from assets.Button import Button
 from typing import List
+import time
 
 pygame.init()
 pygame.font.init()
-
-WWIDTH = 900 # Window Width
-WHEIGHT = 600 # Window Height
-
-FONT = pygame.font.Font('API101/Open Trivia API/assets/Lexend-Regular.ttf', 18) # Main Font
-
-screen = pygame.display.set_mode((WWIDTH, WHEIGHT))
-
-master_asset_list = []
 
 ### WINDOWS ###
 
@@ -21,7 +13,35 @@ MAIN_MENU = 'main_menu'
 CONFIG_MENU = 'config menu'
 GAME_MENU = 'game menu'
 
-def switch_window(window, asset_list:List[Button] = master_asset_list):
+WWIDTH = 900 # Window Width
+WHEIGHT = 600 # Window Height
+
+screen = pygame.display.set_mode((WWIDTH, WHEIGHT))
+
+# GLOBAL / DEFAULT FAUNT # 
+
+FONT = pygame.font.Font('API101/Open Trivia API/assets/Lexend-Regular.ttf', 18) # Main Font
+
+### Master Asset Tracker ###
+
+master_asset_list = []
+
+def append_asset(asset:list)->list:
+    global master_asset_list
+
+    for element in asset:
+        master_asset_list.append(element)
+
+def del_asset(asset:list)->None: ##### [button 1, button 2] --------------- [button4, button2, button3, button1]
+    for i in range(len(asset)):
+        counter = 0                          ############ NOT FUNCTIONAL ################
+        for j in master_asset_list:
+            if j == asset[i]:
+                master_asset_list.pop(counter)
+            counter += 1
+    return master_asset_list
+
+def switch_window(window, asset_list:List[Button] = master_asset_list): ### ONGOING (ESSENTIAL) ###
 
     for element in asset_list:
         if isinstance(Button, element):
@@ -57,22 +77,6 @@ def screen_fill(img):
         log.warning('display Surface quit')
     return
 
-def append_asset(asset:list)->list:
-    global master_asset_list
-
-    for element in asset:
-        master_asset_list.append(element)
-
-    return master_asset_list
-
-def del_asset(asset:list)->None:
-    
-    for i in range(len(asset)):
-        for j in master_asset_list:
-            if j == asset[i]:
-                master_asset_list.pop(i)
-    return master_asset_list
-
 ### Button Components ###
 
 MAIN_BTN_WIDTH = 150
@@ -85,3 +89,56 @@ main_quit_btn = Button(WWIDTH/2 - MAIN_BTN_WIDTH/2, WHEIGHT/2 + 50, 150, 70, 'Qu
 # Quit Game Options
 quit_confirm_yes = Button(WWIDTH/2 - MAIN_BTN_WIDTH/2 - 220, WHEIGHT/2 + 50, 150, 70, 'Yes', FONT, (0,0,0), (250, 250, 250), (100,100,100))
 quit_confirm_no = Button(WWIDTH/2 - MAIN_BTN_WIDTH/2 + 220, WHEIGHT/2 + 50, 150, 70, 'No', FONT, (0,0,0), (250, 250, 250), (100,100,100))
+
+### Support Functions ###
+
+# Visibility / Tracking #
+
+def make_visible(stack:List[Button])->None:
+    if len(stack) < 1:
+        return
+    
+    for button in stack:
+        if not isinstance(button, Button):
+            raise TypeError(f'Type: {str(type(button))} not accepted. Stack must contain only button instances')
+        button.visible = True
+    
+    append_asset(stack)
+    
+
+def make_invisible(stack:List[Button])->None:
+    if len(stack) < 1:
+        return
+    
+    for button in stack:
+        if not isinstance(button, Button):
+            raise TypeError(f'Type: {str(type(button))} not accepted. Stack must contain only button instances')
+        button.visible = False
+
+    del_asset(stack)
+
+def draw_many(stack:List[Button], screen)->None:
+    if len(stack) < 1:
+        return
+    
+    for button in stack:
+
+        if not isinstance(button, Button):
+            raise TypeError(f'Type: {str(type(button))} not accepted. Stack must contain only button instances')
+        button.draw(screen)
+        
+    append_asset(stack)
+
+# Quitting #
+
+def quit_command_ex(event):
+
+    if quit_confirm_yes.isclicked(event):
+        log.info('Ending game...')
+        return True
+    
+    if quit_confirm_no.isclicked(event):
+        log.info('Action retracted, canceling...')
+        make_visible([main_start_btn, main_quit_btn])
+        make_invisible([quit_confirm_yes, quit_confirm_no])
+        return False
--- API101/Open Trivia API/main.py
+++ API101/Open Trivia API/main.py
@@ -3,25 +3,30 @@ from assets.components import *
 from url_request import get_trivia, CROSS_MARK, CHECK_MARK
 import logging as log
 
-def quit_command_ex(event):
+# def quit_command_ex(event):
     
-    if quit_confirm_yes.isclicked(event):
-        log.info('Ending game...')
+#     if quit_confirm_yes.isclicked(event):
+#         log.info('Ending game...')
 
-        return True
+#         return True
 
-    if quit_confirm_no.isclicked(event):
-        log.info('Action retracted, canceling quit...')
-        quit_confirm_yes.visible = False
-        quit_confirm_no.visible = False
-        main_start_btn.visible = True
-        main_quit_btn.visible = True
+#     if quit_confirm_no.isclicked(event):
+#         log.info('Action retracted, canceling...')
+#         quit_confirm_yes.visible = False
+#         quit_confirm_no.visible = False
+#         main_start_btn.visible = True
+#         main_quit_btn.visible = True
 
-        return False
+#         return False
 
 current_window = MAIN_MENU
 
+
+
 def main():
+    global current_window
+    global master_asset_list
+    start_time = time.time()
 
     pygame.display.set_caption('Trivia Game')
     
@@ -29,14 +34,19 @@ def main():
         log.info(f'Main background image; Path: {main_img_path} loaded... OK {CHECK_MARK}')
     except FileNotFoundError:
         log.error(f'Main background image; Path: {main_img_path} failed to load... {CROSS_MARK}')
-    
-    quit_confirm_yes.visible = False
-    quit_confirm_no.visible = False
 
     quit_flag = False
 
     running = True
     while running:
+
+        # TIME TRACKER
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+
+        if elapsed_time >= 1.0:
+            log.info(master_asset_list)
+            start_time = current_time
 
     ### Terminate Game Event ###
 
@@ -51,40 +61,40 @@ def main():
     ### Main Button Events ###
 
         if current_window == MAIN_MENU:
-
-            main_start_btn.draw(screen)
-            main_quit_btn.draw(screen)
             
-            append_asset(main_start_btn, main_quit_btn)
 
-            if main_start_btn.isclicked(event): # Start Game Button
+            # Draw any possible usable buttons within the **main menu** window. Not all will be visible as some Button's
+            # visibility attributes are False
+            draw_many([main_start_btn, main_quit_btn, quit_confirm_yes, quit_confirm_no], screen)
+            # Start with visibility to quit confirmation buttons = False
+            # rest of buttons are visible by default.
+            make_invisible([quit_confirm_yes, quit_confirm_no])
+
+
+            
+            if main_start_btn.isclicked(event): # Start Game Button ### CONTINUE HERE
                 log.info(f'Start Button Clicked {CHECK_MARK}')
                 current_window = CONFIG_MENU
 
-                switch_window(1, master_asset_list)
+                switch_window(1, master_asset_list) ### NOT IMPLEMENTED ###
                 
 
             if main_quit_btn.isclicked(event): # Quit Game Event
                 log.info(f'Quit Button Clicked {CHECK_MARK}')
 
-                main_start_btn.undraw()
-                main_quit_btn.undraw()
-                del_asset([main_start_btn, main_quit_btn])
-
-                quit_confirm_yes.visible = True
-                quit_confirm_no.visible = True
-                append_asset([quit_confirm_yes, quit_confirm_no])
+                make_invisible([main_start_btn, main_quit_btn])
+                make_visible([quit_confirm_yes, quit_confirm_no])
 
                 quit_flag = True
             
             # Draw buttons if attribute, visible = True, else return None and cotinue
-            quit_confirm_yes.draw(screen)
-            quit_confirm_no.draw(screen)
-            append_asset([quit_confirm_yes, quit_confirm_no])
+
+            # append_asset([quit_confirm_yes, quit_confirm_no])
             
             if quit_flag:
                 if quit_command_ex(event):
                     running = False
+
         elif current_window == CONFIG_MENU:
             pass
         elif current_window == GAME_MENU:
@@ -93,3 +103,5 @@ def main():
 
     pygame.quit()
     log.info('Game terminated.')
+
+main()
